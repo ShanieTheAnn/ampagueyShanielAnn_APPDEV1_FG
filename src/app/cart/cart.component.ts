c743641a211e58f647280a7972f15533851a4a96
ShanieTheAnn <shanielann02@gmail.com>
2025-09-17 17:38:31 +0800

    third commit

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CART } from '../shared/cart-data';
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +9,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
-products = [
+export class CartComponent implements OnInit {
+  cartItems = CART;
+  total = 0;
+
+  products = [
     {id: 1, name: "Red Paint", price: 100, stock: 100},
     {id: 2, name: "Blue Paint", price: 100, stock: 0},
     {id: 3, name: "Green Paint", price: 100, stock: 58},
     {id: 4, name: "Invisible Paint", price: 10, stock: 75}
   ];
+  
 
+  ngOnInit() {
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    this.total = this.cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }
+
+   clearCart() {
+    this.cartItems.length = 0;
+
+    this.calculateTotal();
+  }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CART } from '../shared/cart-data';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
@@ -14,6 +16,7 @@ import { RouterModule } from '@angular/router';
 export class ProductListComponent implements OnInit {
   productId: number | null = null;
   product: any = null;
+  searchTerm: string = '';
 
   products = [
     {id: 1, name: "Red Paint", price: 100, stock: 100},
@@ -27,5 +30,26 @@ export class ProductListComponent implements OnInit {
   ngOnInit() {
     this.productId = Number(this.route.snapshot.paramMap.get('id'));
     this.product = this.products.find(p => p.id === this.productId);
+  }
+
+  get filteredProducts() {
+    const term = this.searchTerm.trim().toLowerCase();
+
+    if (!term) {
+      return this.products;
+    }
+
+    return this.products.filter(product =>
+      product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  addToCart(product: any) {
+    const existing = CART.find(item => item.id === product.id);
+    if (existing) {
+      existing.quantity += 1;
+    } else {
+      CART.push({ ...product, quantity: 1 });
+    }
   }
 }
